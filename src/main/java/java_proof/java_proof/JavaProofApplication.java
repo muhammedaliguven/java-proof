f6c16fceb8e6333571488285bbab127d96f8fe18
muhammedaliguven <muhammed_ali_000@outlook.com>
2024-09-22 13:43:17 +0300

    First commit

---
+++ src/main/java/java_proof/java_proof/JavaProofApplication.java
@@ -0,0 +1,13 @@
+package java_proof.java_proof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java_proof/java_proof/JavaProofApplicationTests.java
@@ -0,0 +1,13 @@
+package java_proof.java_proof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
